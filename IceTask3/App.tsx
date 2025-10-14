c5cfa4db85c6c20ef62585848e557045f13da05f
Liehan Els <102226621+liehanels@users.noreply.github.com>
2025-10-14 18:10:07 +0200

    Update App.tsx

--- IceTask3/App.tsx
+++ IceTask3/App.tsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#da2222ff',
-    alignItems: 'center',
+    alignItems: 'left',
     justifyContent: 'center',
   },
 });
